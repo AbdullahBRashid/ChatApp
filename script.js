ddebc1eda55797cad59127f320f7bc9c693925a1
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2023-05-02 23:06:57 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let messagesDiv = document.getElementById('messages-div')
+let chatInput = document.getElementById('chat-text')
+
+let websocket = new WebSocket('ws://localhost:4000')
+// var socket=io()
+// // make connection with server from user side
+// socket.on('connect', function(){
+//   console.log('Connected to Server')
+ 
+// });
+
+function sendMessage() {
+    let message = chatInput.value
+    let messageElement = document.createElement('p')
+
+    websocket.send(message)
+    messageElement.classList.add('message')
+    messageElement.classList.add('message-sent')
+    messageElement.innerHTML = `You: ${message}`
+    messagesDiv.appendChild(messageElement)
+    chatInput.value = ''
+}
+
+websocket.addEventListener('message', (message) => {
+    
+    let messageElement = document.createElement('p')
+    messageElement.classList.add('message')
+    messageElement.classList.add('message-got')
+    messageElement.innerHTML = `Them: ${message.data}`
+    messagesDiv.appendChild(messageElement)
+});
+
